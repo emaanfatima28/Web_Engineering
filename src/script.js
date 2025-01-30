7a0329e8ae9818f30a799c3083d7e1d01b25884b
Emaan Fatima <emaanfatima0613@gmail.com>
2025-01-30 13:49:27 +0500

    Lecture 4

---
+++ src/script.js
@@ -0,0 +1,40 @@
+let fruits = new Array("Apple", "Banana", "Orange");
+
+//Array
+// fruits.splice(indexValue, noOfElements, value);
+//fruits.splice(1, 1, "Apple");
+//console.log(fruits);
+//console.log(value.join('-'));
+//let result = fruits.indexOf("Apricot");
+//console.log(result);
+
+//Filter and forEach not so useful
+//console.log(fruits.forEach((value) => val.toUpperCase()));
+//console.log(fruits.filter(val => val === "Apple"));
+
+//Map
+//console.log(fruits.map(val => "<li>" + val + "<li>"));
+
+//Reduce
+//currentValue,Definition
+// let values = [1,2,5,8,9];
+// let result = values.reduce(((acc,val) => acc * val),0);
+// console.log(result);
+
+// let result = fruits.reduce((acc,val) => acc + "<li>" + val +"<li>","<ul>");
+// result += "<ul>";
+// console.log(result);
+
+// let number = [1,2,3,4,8,0];
+// let max = num.reduce(((acc,val)=> acc > val ) acc : val),number[0]);
+
+//Set
+//size,has,add,delete
+// let value = new Set[1,2,1];
+// console.log(value);
+
+//String
+// let value = 3;
+//let print = "Hello" + value;
+// let displayMessage = 'Hello ${value}';
+// console.log(displayMessage);
